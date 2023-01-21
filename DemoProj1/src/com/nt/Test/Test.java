c0f9b737082cc137f79f1f91f1a6ca2759347a13
TL <TL@gmail.com>
2023-01-21 16:07:42 +0530

    dev1 modified test.java

--- DemoProj1/src/com/nt/Test/Test.java
+++ DemoProj1/src/com/nt/Test/Test.java
@@ -1,5 +1,11 @@
 package com.nt.Test;
 
+import com.nt.bean.JavaUtility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		JavaUtility u=new JavaUtility();
+		System.out.println(u.getDate());
+	}
 }
