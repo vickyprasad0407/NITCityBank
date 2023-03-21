4d76c35fd607ada31449cc344cfac1f7217f15a7
Tl <TL@gmail.com>
2023-03-21 05:36:13 +0530

    checking conflict

---
+++ DemoProj1/src/com/nt/bean/Branch7.java
@@ -0,0 +1,5 @@
+package com.nt.bean;
+
+public class Branch7 {
+
+}
---
+++ DemoProj1/src/com/nt/bean/Conflict.java
@@ -0,0 +1,10 @@
+package com.nt.bean;
+
+public class Conflict {
+
+	public static void main(String[] args) {
+
+		System.out.println("Conflict.main()");
+	}
+
+}
